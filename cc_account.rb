7e151acc667af924a340da2b38eb5c971a75c8f6
jettkendrick <jettkendrick@gmail.com>
2019-03-13 20:28:24 -0500

    trying to fix indentation issues caused by rubocop

--- cc_account.rb
+++ cc_account.rb
@@ -3,73 +3,63 @@ class Creditcard
   attr_reader :apr, :limit, :opened 
   attr_accessor :balance, :transactions, :interest, :activity, :day
 
-	def initialize(apr, limit, _date = Date.today)
-		@apr = (apr / 100.0 / 365)
-		@interest = 0
-		@limit = limit
-		@balance = 0
-		@transactions = {}
-		@day = 0
-		@opened = Date.today.to_s
-		@activity = @transactions[@opened] = []
-	end
+  def initialize(apr, limit, _date = Date.today)
+	   @apr = (apr / 100.0 / 365)
+		  @interest = 0
+		  @limit = limit
+		  @balance = 0
+		  @transactions = {}
+		  @day = 0
+		  @opened = Date.today.to_s
+		  @activity = @transactions[@opened] = []
+	 end
 
-	def day_progess(days)
-		@day = days
-		if @balance > 0 
-			@interest += balance * @apr * days
-		end
-	end 
+	 def day_progess(days)
+		  @day = days
+		  if @balance > 0 
+		    @interest += balance * @apr * days
+		  end
+	 end 
 
-	def add_charge(amount)
-		unless @balance + amount > @limit
-			@balance += amount
-		else 
-			raise ArgumentError.new("Over Limit!")
-		end
-		@opened = (Date.today + @day).to_s
-		if @transactions.has_key?(@opened)
-		  @activity << -amount
+	 def add_charge(amount)
+		  unless @balance + amount > @limit
+			   @balance += amount
+		 else 
+			  raise ArgumentError.new("Over Limit!")
+		  end
+		  @opened = (Date.today + @day).to_s
+		  if @transactions.has_key?(@opened)
+		    @activity << -amount
 		else
 		  @activity = @transactions[@opened] = []
 		  @activity << -amount
-		end  
-	end
+		  end  
+	 end
    
   def add_payment(amount)
-		@balance -= amount
-		@opened = (Date.today + @day).to_s
-		if @transactions.has_key?(@opened)
-		  @activity << amount
+		  @balance -= amount
+		  @opened = (Date.today + @day).to_s
+		  if @transactions.has_key?(@opened)
+		    @activity << amount
 		else
 		  @activity = @transactions[@opened] = []
-			@activity << amount
-		end
-	end
+			 @activity << amount
+		  end
+	 end
 
-	def view_activity 
-		p @transactions
-	end 
+	 def view_activity 
+		  p @transactions
+	 end 
 
 
-	def check_balance(day_number)
-		if day_number % 30 == 0 
+	 def check_balance(day_number)
+		  if day_number % 30 == 0 
      	(balance + interest).round(2)
    	else
       balance.round(2)
     end 
   end
 end
-
-credit = Creditcard.new(35, 1000)
-credit.add_charge(500)
-credit.day_progess(15)
-credit.add_payment(200)
-credit.add_charge(50)
-credit.day_progess(10)
-credit.add_charge(100)
-credit.day_progess(5)
-credit.check_balance(30)
 
 
 
--- cc_account_test.rb
+++ cc_account_test.rb
@@ -4,23 +4,23 @@ require './cc_account'
 
 class CardTest < Minitest::Test
 
-	def test_scenario_1
-		credit = Creditcard.new(35, 1000)
-		credit.add_charge(500)
-		credit.day_progess(30)
-		assert_equal 514.38, credit.check_balance(30)
-	end		
+  def test_scenario_1
+	   credit = Creditcard.new(35, 1000)
+		  credit.add_charge(500)
+		  credit.day_progess(30)
+		  assert_equal 514.38, credit.check_balance(30)
+	 end		
 	
-	def test_scenario_2 
-		credit = Creditcard.new(35, 1000)
-		credit.add_charge(500)
-		credit.day_progess(15)
-		credit.add_payment(200)
-		credit.day_progess(10)
-		credit.add_charge(100)
-		credit.day_progess(5)
-		assert_equal 411.99, credit.check_balance(30)
-	end
+	 def test_scenario_2 
+		  credit = Creditcard.new(35, 1000)
+		  credit.add_charge(500)
+		  credit.day_progess(15)
+		  credit.add_payment(200)
+		  credit.day_progess(10)
+		  credit.add_charge(100)
+		  credit.day_progess(5)
+		  assert_equal 411.99, credit.check_balance(30)
+	 end
 
   def test_create_account
     credit = Creditcard.new(35, 1000)
